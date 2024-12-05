7d001fa0efd20a62693c469ed12af169f8005e9c
Deyvid-Genu <deyvid.genu@gmail.com>
2024-12-05 12:53:44 -0300

    rewritten the ScrollAnimation class

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -3,23 +3,44 @@ export default class ScrollAnimation {
     this.sections = document.querySelectorAll(sections);
     this.halfWindow = window.innerHeight * 0.6;
 
-    this.scrollAnimation = this.scrollAnimation.bind(this);
+    //binding the oject as the this of the callback function
+    this.checkDistance = this.checkDistance.bind(this);
   }
-  scrollAnimation() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const visibleSection = sectionTop - this.halfWindow < 0;
-      if (visibleSection) {
-        section.classList.add('ativo');
-      } else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
+
+  //get the offsetTop of each element
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.halfWindow),
+      };
+    });
+  }
+
+  //compare the offsetTop of the elements with the window scrollY
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.scrollY > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     });
   }
+
+  //initate the functions that get the elements offsetTop, the function that compare the element offsetTop with the defined distance in the window scrollY and start the animation
   init() {
     if (this.sections.length) {
-      this.scrollAnimation();
-      window.addEventListener('scroll', this.scrollAnimation);
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
     }
+    return this;
+  }
+
+  //remove the event when scrolling
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
