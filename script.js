5dd15189038eea3ee90a3e147678331d6b818055
Deyvid-Genu <deyvid.genu@gmail.com>
2024-12-11 20:36:49 -0300

    pictures slide added

--- script.js
+++
@@ -1,2 +0,0 @@
-let texto =
-  'lorem  ijdshuiucjnjdncodocjoijdijcoijojeiciojoijoijoijiojijoijoijoijoijijojijijijiojiojijoijoijoijijiojiojoijiojoijoijjoijoijjojijoijoijoijiojijijijijojoijijoijoijiojijiojijiojiojijijijjijijoijojijojirjrojijijgy';
